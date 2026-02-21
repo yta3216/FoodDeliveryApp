8dff227ca8bb0620ee6dd8c8c179d3d8452b3ec8
Owen <owenjameswood@icloud.com>
2026-02-20 18:25:35 -0800

    modified basic user functionality

--- fullstack-project/backend/app/services/user_service.py
+++ fullstack-project/backend/app/services/user_service.py
@@ -1,11 +1,12 @@
+"""This module implements business logic for user management."""
+
 import uuid
-from typing import List
 from fastapi import HTTPException
 from app.repositories.user_repo import load_users, save_users
-from app.schemas.user_schema import user
+from app.schemas.user_schema import User
 
 
-def create_user(payload: user) -> user:
+def create_user(payload: User) -> User:
     users = load_users()
     new_id = str(uuid.uuid4()) # generate unique ID for the new user
     if any(user.get("id") == new_id for user in users):
@@ -19,3 +20,9 @@ def create_user(payload: user) -> user:
     save_users(users)
     return new_user
 
+def get_user_by_id(user_id: str) -> User:
+    users = load_users()
+    for user in users:
+        if user.get("id") == user_id:
+            return User(**user)
+    raise HTTPException(status_code=404, detail=f"User '{user_id}' not found")
