313214701e836c472d0ae986432e81048c2ee04c
Owen <owenjameswood@icloud.com>
2026-03-05 01:11:22 -0800

    added some notes for myself on cart_router.py

--- fullstack-project/backend/app/routers/cart_router.py
+++ fullstack-project/backend/app/routers/cart_router.py
@@ -2,4 +2,4 @@
 
 # TODO: make sure there is a router for deleting a cart, which would just be setting the cart to a new Cart() object (restores defaults)
 
-# TODO: ensure each method calls require_role for Customer
+# TODO: ensure each method calls require_role for Customer, to both ensure role is there and that they're logged in. Cast output as Customer
