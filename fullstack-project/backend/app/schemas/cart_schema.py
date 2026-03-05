5b8dd0c33d94a984977704eebca252513ea8c6a9
Owen <owenjameswood@icloud.com>
2026-03-04 23:17:32 -0800

    added cart schema

---
+++ fullstack-project/backend/app/schemas/cart_schema.py
@@ -0,0 +1,39 @@
+"""
+This module defines the basic cart schema for the application.
+
+Any updates to the cart details should follow this schema.
+"""
+
+from typing import List
+from pydantic import BaseModel
+
+# generic cart item schema. we store the MenuItem ID rather than the item itself in the cart to save storage.
+class CartItem(BaseModel):
+    menu_item_id: int
+    qty: int
+
+# schema to add an item to cart
+class CartItem_Create(BaseModel):
+    menu_item_id: int
+    qty: int
+
+# schema to update cart item qty
+class CartItem_Update(BaseModel):
+    menu_item_id: int
+    qty: int
+
+# schema to remove item from cart
+class CartItem_Delete(BaseModel):
+    menu_item_id: int
+
+# generic cart schema
+class Cart(BaseModel):
+    menu_id: int
+    cart_items: List[CartItem]
+
+# schema to create cart
+class Cart_Create(BaseModel):
+    menu_id: int
+    cart_items: List[CartItem]
+
+# no schema needed to delete cart, as no data is required
